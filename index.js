f39744dda8f8afe726a97a86893f7ff07b00a8fa
cassieview <cassie.siljander@microsoft.com>
2019-04-24 16:10:06 -0500

    addSpheres Method and button to addSpheres

--- index.js
+++ index.js
@@ -18,18 +18,11 @@ function createScene() {
     // Add Light
     var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene);
 
-    
+
     // Enable Physics and set gravtiy force with a vector
     var gravityVector = new BABYLON.Vector3(0, -10, 0);
     scene.enablePhysics(gravityVector, new BABYLON.CannonJSPlugin());
 
-    // Create sphere
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 5 }, scene);
-    sphere.position.y = 10;
-    sphere.position.x = -10;
-    sphere.position.z = -10;
-    sphere.material = new BABYLON.StandardMaterial("sphere material", scene);
-    sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
 
     // Create Ground
     var ground = BABYLON.Mesh.CreateBox("Ground", 1, scene);
@@ -44,13 +37,38 @@ function createScene() {
     groundMat.backFaceCulling = false;
     ground.material = groundMat;
     ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, friction: 0.5, restitution: 0.7 }, scene);
-  
+
+     // GUI
+     var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+
+     var button = BABYLON.GUI.Button.CreateSimpleButton("button", "Start Game");
+     button.width = 0.2;
+     button.height = "40px";
+     button.color = "white";
+     button.background = "blue";
+     button.onPointerUpObservable.add(function () {
+        addSpheres(scene,10);
+        button.isVisible = false;
+    });
+    advancedTexture.addControl(button);  
+
     // Enable VR
-    var vrHelper = scene.createDefaultVRExperience();
-    vrHelper.enableInteractions();
+    // var vrHelper = scene.createDefaultVRExperience();
+    // vrHelper.enableInteractions();
 
     return scene;
 }
+
+var addSpheres = function (scene, amount) {
+
+    for (let index = 0; index < amount; index++) {
+        var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
+        sphere.position = new BABYLON.Vector3(Math.random() * 20 - 10, 10, Math.random() * 10 - 5);
+        sphere.material = new BABYLON.StandardMaterial("sphere material", scene)
+        sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1 }, scene);
+    }
+}
+
 
 var scene = createScene();
 
